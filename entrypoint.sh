ce23ca6e2d89795fe2fa3d1e826ff7e9d1f7ccf5
anarcher <anarcher@gmail.com>
2019-10-03 01:20:31 +0900

    /opt/hostedtoolcache

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ echo "Hello! $1"
 echo "pwd: $(pwd)"
 echo "$PATH"
 echo "RUNNER_TOOL_CACHE: $RUNNER_TOOL_CACHE"
+echo "toolcache: $(ls -la /opt/hostedtoolcache)"
 echo "ls: $(ls -la)"
 time=$(date)
 echo ::set-output name=time::$time
