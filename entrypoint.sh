070602c73ab595f63287769ec08e0663e34c78f5
anarcher <anarcher@gmail.com>
2019-10-03 01:26:12 +0900

    github/home

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,7 @@ echo "$PATH"
 echo "RUNNER_TOOL_CACHE: $RUNNER_TOOL_CACHE"
 echo "toolcache: $(ls -la /opt/hostedtoolcache)"
 echo "ls: $(ls -la)"
+echo " $(ls -la /github/home)"
+echo " $(ls -la /github/workflow)"
 time=$(date)
 echo ::set-output name=time::$time
