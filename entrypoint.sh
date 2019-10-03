296c077af3bba545864ca4e3ff4ccfd7b7aeb5c2
anarcher <anarcher@gmail.com>
2019-10-03 23:24:21 +0900

    test1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,4 @@
 #!/bin/sh -l
 
-echo "Hello! $1"
-echo "pwd: $(pwd)"
-echo "$PATH"
-echo "RUNNER_TOOL_CACHE: $RUNNER_TOOL_CACHE"
-echo "toolcache: $(ls -la /opt/hostedtoolcache)"
-echo "ls: $(ls -la)"
-echo " $(ls -la /github/home)"
-echo " $(ls -la /github/workflow)"
-time=$(date)
-echo ::set-output name=time::$time
+echo "Hello $1"
+echo "Hello2 $*"
